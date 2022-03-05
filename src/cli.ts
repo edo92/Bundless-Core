b20c38dd05b61c797f3007b0b1b8c28f6afcab8f
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2022-03-04 18:43:18 -0800

    cli commands

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+import { Command, Option } from 'commander';
+
+export class Cli {
+    private commander: Command;
+
+    public get options() {
+        return this.commander.parse().opts();
+    }
+
+    constructor() {
+        this.commander = new Command();
+        this.register();
+    }
+
+    private register(): void {
+        this.commander.addOption(new Option('-e, --entry [string...]', 'entry points'));
+        this.commander.addOption(new Option('-n, --name <string>', 'archive zip fie name'));
+        this.commander.addOption(new Option('-o, --outDir <string>', 'out directory'));
+        this.commander.addOption(new Option('-m, --minify <boolean>', 'minify module'));
+        this.commander.addOption(
+            new Option('-ex, --external [string...]', 'external modules')
+        );
+    }
+}
