023d90c6f021a87185feb709973526e1f22b2c38
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2022-03-11 05:43:11 -0800

    installer lib

--- src/lib/installer.ts
+++ src/lib/installer.ts
@@ -1,18 +1,9 @@
-import * as path from 'path';
-import * as fs from 'fs-extra';
 import * as cp from 'child_process';
 
-export class Installer {
-    private cmd: string;
+export function Installer(dirpath: string) {
+    const cmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
 
-    constructor(private dirpath: string) {
-        const platform = process.platform;
-        this.cmd = platform === 'win32' ? 'npm.cmd' : 'npm';
-    }
-
-    public install(): void {
-        cp.spawnSync(this.cmd, ['install --production'], {
-            cwd: this.dirpath,
-        });
-    }
+    cp.spawnSync(cmd, ['install'], {
+        cwd: dirpath,
+    });
 }
